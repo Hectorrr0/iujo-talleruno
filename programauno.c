ddc1872e83ab65b6f31b5c5e8dcae0043ad87a6b
Hector <thehectorr.05@gmail.com>
2024-06-30 15:39:55 -0400

    desarrollando la logica

--- programauno.c
+++ programauno.c
@@ -13,6 +13,25 @@ typedef struct {
     int maltasDetal;
 } Cliente;
 
+/*DESARROLLANDO LA LOGICA*/
+
+float calcularTotal(Cliente cliente) {
+    float total = 0;
+    if (cliente.refrescosDetal > 6) {
+        total += cliente.refrescosDetal * 5; // al mayor
+    } else {
+        total += cliente.refrescosDetal * 10; //al detal
+    }
+
+    if (cliente.maltasDetal > 6) {
+        total += cliente.maltasDetal * 7; //al mayor
+    } else {
+        total += cliente.maltasDetal * 12; //al detal
+    }
+
+    return total;
+}
+
 
 int main() {
 	
