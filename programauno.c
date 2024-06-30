7a81cac5d7e3656828d0b838268db6cce864244b
Hector <thehectorr.05@gmail.com>
2024-06-30 15:38:05 -0400

    definiendo las variables

--- programauno.c
+++ programauno.c
@@ -3,6 +3,17 @@
 
 /*PROGRAMAA*/
 
+
+/*DEFINIENDO LAS VARIABLES*/
+
+typedef struct {
+    char razonSocial[100];
+    char cedula[20];
+    int refrescosDetal;
+    int maltasDetal;
+} Cliente;
+
+
 int main() {
 	
 	return 0;
